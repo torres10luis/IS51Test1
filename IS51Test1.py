7e2705b531d36b68291b3cb59b0c35ac752ddfba
luis saldana <tluis10a@gmail.com>
2020-11-12 17:26:17 -0800

    structured english complete

---
+++ IS51Test1.py
@@ -0,0 +1,14 @@
+""" 
+Structured-English branch
+
+Starting with this problem we can see that we have 2 salary options which consists of 10 days worked. as it explains with option 1 is given a $100 dollars per day for the 10 days of work. As for the second option we are given with a choice of receiving $1 the first day and then it doubles until you reach the 10th day of work. With this program we will be able to determine with of these options is better and we will also be calculating in 2 different functions to determine which option is better at the 10th day.
+
+if option 1 is better it will print in the console that option 1 is better.
+if option 2 is better it will print in the console that option 2 is better.
+but if both options pay the same amount of money in the 10th day it will print out in the console that both option 1 and option 2 will pay the same amount of money at the 10th day
+
+function one is simpler because it will consist of $100 *  10 which is what is said in the description.
+function two will be a more complex because it will have to loop 10 times to represent the 10 days in total and doubling the amount each day till end of 10th day then it will sum everything up
+
+
+ """
